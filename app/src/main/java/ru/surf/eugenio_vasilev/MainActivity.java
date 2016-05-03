cff05a9316e902572d106c43c380db07cd3624aa
Eugenio Vasilev <s.feech@gmail.com>
2016-05-03 16:58:36 +0300

    Добавлен ListView для формирования списка и файл со структурой элемента ListView. Создан адаптер для кастомизации этого списка.  Цвета для элементов списка хранятся в цветовой модели hsv

--- app/src/main/java/ru/surf/eugenio_vasilev/MainActivity.java
+++ app/src/main/java/ru/surf/eugenio_vasilev/MainActivity.java
@@ -2,12 +2,33 @@ package ru.surf.eugenio_vasilev;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<float[]> hsvColorsList = new ArrayList<float[]>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // создание адаптера
+        createHsvColorList(); // создание списка hsv цветов
+        LvAdapter lvAdapter = new LvAdapter(this, hsvColorsList);
+
+        // настраиваем список
+        ListView lvMain = (ListView) findViewById(R.id.lvMain);
+        lvMain.setAdapter(lvAdapter);
     }
+
+    private void createHsvColorList(){
+        for(float i = 0; i < 360; i++){
+            hsvColorsList.add(new float[]{i, 1, 1});
+        }
+    }
+
 }
